d20de7455bde2c6c014e710de52363555fca4105
Alex Nastin <aliaksei.nastsin@netcracker.com>
2018-01-23 21:14:33 +0300

    Init commit

---
+++ src/main/java/by/bsuir/dissertation/DissertationPart2Application.java
@@ -0,0 +1,12 @@
+package by.bsuir.dissertation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DissertationPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DissertationPart2Application.class, args);
+	}
+}
